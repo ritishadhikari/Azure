525d3371b43bdb21c538dfae9f3fbc0100ef8b4a
Ritish Adhikari <ritishadhikari@gmail.com>
2023-09-20 18:51:51 +0530

    Till 09/20/2023 18:51:51

--- Microsoft_Azure_Data_Scientist_Associate_OfficialPage/4_1_1_train-model-parameters.py
+++ Microsoft_Azure_Data_Scientist_Associate_OfficialPage/4_1_1_train-model-parameters.py
@@ -10,57 +10,71 @@ import matplotlib.pyplot as plt
 
 
 def main(args):
-    df=get_data(path=args.training_data)
-    X_train, X_test, y_train, y_test=split_data(df=df)
-    model=train_model(reg_rate=args.reg_rate,
-                      X_train=X_train,
-                      y_train=y_train)
-    evaluate_model(model=model,
-                   X_test=X_test,
-                   y_test=y_test)
+    df = get_data(path=args.training_data)
+    X_train, X_test, y_train, y_test = split_data(df=df)
+    model = train_model(reg_rate=args.reg_rate, X_train=X_train, y_train=y_train)
+    evaluate_model(model=model, X_test=X_test, y_test=y_test)
+
 
 def parse_args():
-    parser=argparse.ArgumentParser()
-    parser.add_argument("--training_data",dest="training_data",type=str)
-    parser.add_argument("--reg_rate",dest="reg_rate",type=float, default=0.01)
-    args=parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--training_data", dest="training_data", type=str)
+    parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.01)
+    args = parser.parse_args()
     return args
 
+
 def get_data(path):
-    df=pd.read_csv(path)
+    df = pd.read_csv(path)
     return df
 
+
 def split_data(df):
-    X= df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness',
-    'SerumInsulin','BMI','DiabetesPedigree','Age']].values 
-    y=df['Diabetic'].values
-    X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.30, random_state=0)
+    X = df[
+        [
+            "Pregnancies",
+            "PlasmaGlucose",
+            "DiastolicBloodPressure",
+            "TricepsThickness",
+            "SerumInsulin",
+            "BMI",
+            "DiabetesPedigree",
+            "Age",
+        ]
+    ].values
+    y = df["Diabetic"].values
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=0
+    )
     return X_train, X_test, y_train, y_test
 
-def train_model(reg_rate,X_train,y_train):
-    model=LogisticRegression(C=1/reg_rate,solver="liblinear")
-    model.fit(X_train,y_train)
+
+def train_model(reg_rate, X_train, y_train):
+    model = LogisticRegression(C=1 / reg_rate, solver="liblinear")
+    model.fit(X_train, y_train)
     return model
 
-def evaluate_model(model,X_test,y_test):
-    y_hat=model.predict(X_test)
-    acc=np.average(y_test==y_hat)
+
+def evaluate_model(model, X_test, y_test):
+    y_hat = model.predict(X_test)
+    acc = np.average(y_test == y_hat)
     print(f"Accuracy:{acc}")
 
-    y_scores=model.predict_proba(X_test)
-    auc=roc_auc_score(y_test, y_scores[:,1])
+    y_scores = model.predict_proba(X_test)
+    auc = roc_auc_score(y_test, y_scores[:, 1])
     print(f"AUC:{auc}")
 
-    fpr, tpr, thresholds=roc_curve(y_test,y_scores[:,1])
-    fig=plt.figure(figsize=(6,4))
-    plt.plot([0,1],[1,0],"k--")
+    fpr, tpr, thresholds = roc_curve(y_test, y_scores[:, 1])
+    fig = plt.figure(figsize=(6, 4))
+    plt.plot([0, 1], [1, 0], "k--")
     plt.plot(fpr, tpr)
     plt.xlabel("False Positive Rate")
     plt.ylabel("True Positive Rate")
-    plt.title('ROC Curve')
+    plt.title("ROC Curve")
 
-if __name__=="__main__":
-    args=parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
     main(args)
     print("*" * 60)
     print("\n\n")
--- Microsoft_Azure_Fundamentals_Udemy/total_lines.py
+++ Microsoft_Azure_Fundamentals_Udemy/total_lines.py
@@ -1,16 +1,16 @@
 import os
 import natsort
 
-location="./Microsoft_Azure_Fundamentals_Udemy/"
+location = "./Microsoft_Azure_Fundamentals_Udemy/"
 files = natsort.natsorted([f for f in os.listdir(location) if f.endswith("txt")])
-size=len(files)
+size = len(files)
 # size=9
-lineCount=0
+lineCount = 0
 for f in files[:size]:
     # print(f)
-    if f!="acc.txt":
+    if f != "acc.txt":
         with open(os.path.join(location, f)) as f:
-            page=f.readlines()
-            lineCount+=len(page)
+            page = f.readlines()
+            lineCount += len(page)
 
 print(lineCount)
