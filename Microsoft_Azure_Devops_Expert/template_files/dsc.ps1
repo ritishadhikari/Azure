5cb956b0bfc79828fb0a70c0bb34db33d3abccef
ritishadhikari <ritishadhikari@gmail.com>
2023-12-24 03:04:36 +0530

    Till 12/24/2023 03:04:36

---
+++ Microsoft_Azure_Devops_Expert/template_files/dsc.ps1
@@ -0,0 +1,20 @@
+Configuration MyDscConfiguration
+{
+    param
+    (
+        [string[]]$ComputerName="Localhost"
+    )
+    Node $ComputerName
+    {
+        WindowsFeature MyFeatureInstance
+        {
+            Ensure="Present"
+            Name="RSAT"
+        }
+        WindowsFeature My2ndFeatureInstance
+        {
+            Ensure="Present"
+            Name="Bitlocker"
+        }
+    }
+}
